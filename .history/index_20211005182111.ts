390484cec16bf3ff9ed99fe77f1f3a4cb22ad8ec
Sindhu Binil <sbtker@gmail.com>
2021-10-05 18:38:47 -0400

    initial commit

---
+++ .history/index_20211005182111.ts
@@ -0,0 +1,42 @@
+import http from 'http';
+import path from 'path';
+import fs from 'fs';
+
+const hostname: string = '127.0.0.1';
+const port: number = 3000;
+
+
+
+console.log(__dirname);
+
+const server: http.Server = http.createServer((req, res) => 
+{
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Everyone, Welcome to my Website!');
+});
+
+
+server.listen(port, hostname, () => 
+{
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+function displayHome(res: http.ServerResponse): void
+{
+  fs.readFile("index.html", (err, data) => 
+
+{
+  if(err)
+  {
+    res.writeHead(404);
+    res.end("ERROR: - Page not found");
+    console.log("Error");
+    return;
+  }
+
+  res.writeHead(200);
+  res.end(data);
+
+})
+}
